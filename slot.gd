f07a709552fdc0b5f8ee6de51d640ff6e25273a1
chluebi <42419603+chluebi@users.noreply.github.com>
2025-10-04 18:47:40 +0200

    potions in slots

--- slot.gd
+++ slot.gd
@@ -1,8 +1,17 @@
 extends Control
 
 var slot_index := 0
+const POTION_SCENE = preload("res://potion.tscn")
 
 @onready var selector: AnimatedSprite2D = $Selector # Assuming $Selector is the path to your AnimatedSprite2D
+@onready var potion := POTION_SCENE.instantiate()
+
+func _ready() -> void:
+	add_child(potion) 
+	potion.position = position
+	potion.set_potion_type(randi_range(0, 6))
+	
+	potion.scale = Vector2(0.9, 0.9)
 
 func select_slot():
 	selector.show() 
