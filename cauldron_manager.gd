70a94c1a8e83d00df770b5c6dca3d03fa3b4ae94
chluebi <42419603+chluebi@users.noreply.github.com>
2025-10-06 12:03:17 +0200

    big map

--- cauldron_manager.gd
+++ cauldron_manager.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"../Environment/cauldron".setup(
+	$"../Environment/cauldron_orange".setup(
 		Constants.item_type.firestone,
 		false,
 		Constants.item_type.empty,
@@ -11,12 +11,44 @@ func _ready() -> void:
 		4.0
 	)
 	
-	$"../Environment/cauldron2".setup(
+	$"../Environment/cauldron_yellow".setup(
 		Constants.item_type.bone,
 		true,
 		Constants.item_type.bone,
 		Constants.item_type.yellow,
 		8.0
+	)
+	
+	$"../Environment/cauldron_pink".setup(
+		Constants.item_type.flower,
+		false,
+		Constants.item_type.empty,
+		Constants.item_type.pink,
+		6.0
+	)
+	
+	$"../Environment/cauldron_blue".setup(
+		Constants.item_type.powder,
+		false,
+		Constants.item_type.empty,
+		Constants.item_type.blue,
+		10.0
+	)
+	
+	$"../Environment/cauldron_purple".setup(
+		Constants.item_type.pink,
+		true,
+		Constants.item_type.mushroom,
+		Constants.item_type.purple,
+		10.0
+	)
+	
+	$"../Environment/cauldron_green".setup(
+		Constants.item_type.blue,
+		true,
+		Constants.item_type.slime,
+		Constants.item_type.green,
+		12.0
 	)
 
 
